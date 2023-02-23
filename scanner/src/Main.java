89c35d3fc4b0b934ae7a8d968facd662df1d0e70
L.H <michael123303@yahoo.com>
2023-02-23 20:45:34 +0800

    2/23

---
+++ scanner/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+//print -> 接續當行
+//println -> 換行輸出
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("輸入國文成績:");
+        int scoreCh = scan.nextInt();
+        System.out.print("輸入英文成績:");
+        int scoreEn = scan.nextInt();
+        System.out.print("輸入數學成績:");
+        int scoreMa = scan.nextInt();
+        int sum = scoreCh + scoreEn + scoreMa;
+        System.out.println("總成績:" + String.valueOf(sum));
+    }
+}
