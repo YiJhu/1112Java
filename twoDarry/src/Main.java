5319cfeddd9686f1ad3211da770a38af8c7743bc
L.H <michael123303@yahoo.com>
2023-04-13 21:19:42 +0800

    3/23

--- twoDarry/src/Main.java
+++ twoDarry/src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         double[][] scores = new double[2][3];
         Scanner scan = new Scanner(System.in);
-        //輸入成績
+        // 輸入成績
         for (int i = 0; i < scores.length; i++) {
             for (int j = 0; j < scores[i].length; j++) {
                 System.out.print("第" + (i + 1) + "班第" + (j + 1) + "位學生成績:");
                 scores[i][j] = scan.nextDouble();
             }
         }
-        //計算各班總分
+        // 計算各班總分
         double[] sums = {0, 0};
         for (int i = 0; i < scores.length; i++) {
             for (int j = 0; j < scores[i].length; j++)
